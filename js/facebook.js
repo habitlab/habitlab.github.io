30756d6ba0fbdc28896d1f4de25f14c99abb2653
Sarah Sukardi <sukardi@jhu.edu>
2016-07-18 17:14:01 -0700

    removed extraneous file

--- js/facebook.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('shareButton').onclick = function() {
-  FB.ui({
-    method: 'share',
-    display: 'popup',
-    href: 'https://developers.facebook.com/docs/',
-  }, function(response){});
-}
-
-console.log("Hello")
