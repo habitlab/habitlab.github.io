63803e62013dadc770ade2e7de57059d9245c444
Geza Kovacs <gkovacs@stanford.edu>
2016-12-25 19:37:38 -0800

    show the swal before the get request completes

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@ function addButtonListener() {
     const emailInput = $('#emailInput').val()
 
     if (emailInput !== '') { //if email is not empty
+      $('#emailInput').val(''); // clears the email input
+      swal({
+        title: 'Thanks for signing up',
+        text: 'Once HabitLab is ready you will get an email at ' + emailInput,
+        type: 'success',
+      });
       $.getJSON(`//habitlab.herokuapp.com/addsignup?${$.param({email: emailInput})}&callback=?`, null, function(response) {
         console.log('response data received');
-        $('#emailInput').val(''); // clears the email input
-        swal({
-          title: 'Thanks for signing up',
-          text: 'Once HabitLab is ready you will get an email at ' + emailInput,
-          type: 'success',
-        });
       });      
     }
   });
