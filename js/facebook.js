579d6831998dffb5b9eaead6fa156357bde9b6fc
Geza Kovacs <gkovacs@stanford.edu>
2017-03-14 16:22:34 -0700

    updated version from lewin

--- js/facebook.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('shareButton').onclick = function() {
-  FB.ui({
-    method: 'share',
-    mobile_iframe: true,
-    href: 'habitlab.stanford.edu',
-  }, function(response){});
-}
