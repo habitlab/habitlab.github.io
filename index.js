5bbea7f61cbe35b517e10add6f0dbbf9c4f6e1a8
Geza Kovacs <gkovacs@stanford.edu>
2017-03-20 11:19:30 -0700

    updated website

--- index.js
+++ index.js
@@ -52,9 +52,9 @@ if (chrome && chrome.app && chrome.webstore && window.location.protocol == 'http
             //$('#install_text_bottom').text('Installing HabitLab')
             //window.location.href = 'https://chrome.google.com/webstore/detail/habitlab/obghclocpdgcekcognpkblghkedcpdgd'
             set_install_button_text('HabitLab has been installed')
-            setTimeout(function() {
-              window.location.href = onboarding_url
-            }, 1000)
+            //setTimeout(function() {
+            //  window.location.href = onboarding_url
+            //}, 2000)
           },
           function() {
             // failure
