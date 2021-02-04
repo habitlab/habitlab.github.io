066155620c48d54d5b5579c5f1d09540a35017e8
Geza Kovacs <gkovacs@stanford.edu>
2021-02-04 13:03:18 -0800

    add document.referrer to init data

--- translations.js
+++ translations.js
@@ -210,7 +210,7 @@ function addLog(eventType, data, callback, timeLimit) {
     data.MtLang = mtLang;
   }
   // user id and session id also send
-  var dataVersion = 6;
+  var dataVersion = 7;
   var time = Date.now();
   var insert_id = generateUUID();
   var rows = {
@@ -234,6 +234,7 @@ var initdata = {
   'Lang': navigator.language,
   'LangList': navigator.languages,
   'Browser': navigator.userAgent,
+  'Ref': document.referrer,
 }
 if (Intl && Intl.DateTimeFormat) {
   initdata.TimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
